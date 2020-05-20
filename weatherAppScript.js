66c360e1148b7d12559a50019987672c01acb1b8
James Busbee <jsb1018@sru.edu>
2020-05-20 18:31:22 -0400

    edited env variables

--- weatherAppScript.js
+++ weatherAppScript.js
@@ -1,21 +1,21 @@
 function getWeather(){
     const summary = document.getElementById("summary");
-    const dsKey = DARK_SKY_KEY;       // api key for DarkSky API
+    const dsKey = $DARK_SKY_KEY;       // api key for DarkSky API
     const dsUrl = "https://api.darksky.net/forecast/";      // api URL
     const degree = "&deg;"                                  // degree symbol
-    
+
     navigator.geolocation.getCurrentPosition(success, error);   // use navigator to retrieve position
-                                                                // pass success and error parameters 
+                                                                // pass success and error parameters
     const f = document.getElementById("fahrenheit");
     const c = document.getElementById("celsius");
     const k = document.getElementById("kelvin");
-    
+
     function success(pos){
         lat = pos.coords.latitude;
         lng = pos.coords.longitude;
         console.log(lat);
         console.log(lng);
-        
+
         // retrieve current location address
         $.ajax({
             url: 'https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json',
@@ -27,22 +27,22 @@ function getWeather(){
                 mode: 'retrieveAddresses',
                 maxresults: '1',
                 gen: '9',
-                apiKey: HERE_KEY
-            }, 
+                apiKey: $HERE_KEY
+            },
             success: function (data) {
                 console.log(data);
                 $("#day").html(data.Response.View[0].Result[0].Location.Address.Label);
             }
         });
-        
-        // retrieve current weather, summary, high/low, skycon 
+
+        // retrieve current weather, summary, high/low, skycon
         $.getJSON(
             dsUrl + dsKey + "/" + lat + "," + lng + "?callback=?",
             function(data){
-                
+
                 // return current day summary
                 $("#summary").html(data.currently.summary);
-                
+
                 // returns temperature in requested format
                 if (f.checked){
                     $("#temp").html(data.currently.temperature.toFixed(0) + degree + " F");
@@ -62,16 +62,16 @@ function getWeather(){
                 else{
                     alert("whoops");
                 }
-                
+
                 // return weekly forecast
                 $("#weeklyForecast").html(data.daily.summary);
                 // JSON object for reference
                 console.log(data);
-                
+
                 const icon = data.currently.icon;
                 const skycon = new Skycons({"color":"orange"});
-                // handles current day skycon selection 
-                switch(icon){ 
+                // handles current day skycon selection
+                switch(icon){
                     case "clear-day":
                         skycon.set("icon", Skycons.CLEAR_DAY);
                         skycon.play();
@@ -115,7 +115,7 @@ function getWeather(){
                 }
             }
         );
-        
+
         // retrieve forecast summary, day names, summaries, high/low, skycons
         $.getJSON(
             dsUrl + dsKey + "/" + lat + "," + lng + "?callback=?",
@@ -127,12 +127,12 @@ function getWeather(){
                     let high = data.daily.data[i].apparentTemperatureHigh;
                     let low = data.daily.data[i].apparentTemperatureLow;
                     let fcTime = data.daily.data[i].time;
-                    
+
                 // assign name of day to forecasts
                     let date = new Date(fcTime * 1000);
                     let dateStr = date.toDateString();
                     $("#dayName"+i).html(dateStr);
-                    
+
                 // returns temperature in requested format
                     if (f.checked){
                     $("#fcHigh"+i).html(high.toFixed(0)+degree+" F");
@@ -151,10 +151,10 @@ function getWeather(){
                 }
                 // list forecasted daily summaries
                     $("#fcSpan"+i).html(fcSummary);
-                
-                // handles assigning skycon 
+
+                // handles assigning skycon
                     let skyconi = new Skycons({"color":"orange"});
-                    switch(iconi){                                  // switch statement to handle skycon selection 
+                    switch(iconi){                                  // switch statement to handle skycon selection
                         case "clear-day":
                             skyconi.set("icon"+i, Skycons.CLEAR_DAY);
                             skyconi.play();
@@ -200,25 +200,25 @@ function getWeather(){
             }
         );
     }
-    
-    function error(){                 // throw error message 
+
+    function error(){                 // throw error message
         summary.innerHTML = "Error retrieving weather forecast.";
     }
 }
 
-// function to display current day and date 
+// function to display current day and date
 function displayTimeAndDay(){
-    
+
     let today = new Date();
     $("#day").html(today.toDateString());
-    
+
 }
 
-// dark mode 
+// dark mode
 function darkMode(){
-    
+
     let spanText = document.getElementById("darkmodesign").innerHTML;
-    
+
     if(spanText == "Dark Mode"){
         $("#container1").css("background-color", "#303030");
         $("#body").css("background-color", "#303030");
